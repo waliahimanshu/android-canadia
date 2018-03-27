7e2e8a52324a1aa1bd0628a8a391ce670b87ae98
Himanshu Walia <himanshu.walia@gmail.com>
2018-03-27 06:37:41 +0200

    adds side nav drawer for filtering

--- mobile-ui/src/main/java/com/waliahimanshu/canadia/ui/home/EERoundOfInvitationsActivity.kt
+++ mobile-ui/src/main/java/com/waliahimanshu/canadia/ui/home/EERoundOfInvitationsActivity.kt
@@ -3,8 +3,14 @@ package com.waliahimanshu.canadia.ui.home
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout
+import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.ProgressBar
 import com.google.firebase.database.*
@@ -26,10 +32,15 @@ class EERoundOfInvitationsActivity : AppCompatActivity() {
 
     private var progressBar: ProgressBar? = null
 
+    private var drawer: DrawerLayout? = null
+
+    private var nvDrawer: NavigationView? = null
+
     companion object {
         fun getLaunchIntent(context: Context): Intent {
             return Intent(context, EERoundOfInvitationsActivity::class.java)
         }
+
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -56,7 +67,56 @@ class EERoundOfInvitationsActivity : AppCompatActivity() {
         database = FirebaseDatabase.getInstance().reference
         progressBar = findViewById(R.id.progress_bar_home)
         progressBar?.visibility = View.VISIBLE
+        drawer = findViewById(R.id.drawer_layout)
+        // Setup drawer view
+
+        // Find our drawer view
+        nvDrawer =  findViewById(R.id.nvView)
+        // Setup drawer view
+        setupDrawerContent(nvDrawer)
+
+//        setupDrawerToggle();
+
+
         loadData()
+
+
+    }
+
+    private fun setupDrawerToggle(): ActionBarDrawerToggle {
+        // NOTE: Make sure you pass in a valid toolbar reference.  ActionBarDrawToggle() does not require it
+        // and will not render the hamburger icon without it.
+        return ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+    }
+    private fun setupDrawerContent(navigationView: NavigationView?) {
+        navigationView?.setNavigationItemSelectedListener { menuItem ->
+            selectDrawerItem(menuItem)
+            true
+        }
+    }
+
+    private fun selectDrawerItem(menuItem: MenuItem) {
+
+        when (menuItem.itemId) {
+            R.id.nav_first_fragment -> {
+
+            }
+            R.id.nav_second_fragment -> {
+
+            }
+            R.id.nav_third_fragment -> {
+
+            }
+            else -> {
+
+            }
+        }
+
+        menuItem.isChecked = true;
+        // Set action bar title
+        title = menuItem.title
+        // Close the navigation drawer
+        drawer?.closeDrawer(GravityCompat.END)
     }
 
     private fun loadData(): ArrayList<RoundOfInvitationModel> {
@@ -93,4 +153,20 @@ class EERoundOfInvitationsActivity : AppCompatActivity() {
     private fun setupRecyclerView(recyclerView: RecyclerView, items: ArrayList<RoundOfInvitationModel>) {
         recyclerView.adapter = RoundOfInvitationsAdapter(this, items, twoPane)
     }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.home_menu_filter, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.menu_filter -> {
+                drawer?.openDrawer(GravityCompat.END)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
 }
